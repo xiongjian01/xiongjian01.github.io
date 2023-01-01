ea67ada578f20e282dd0ec195ae745c6891218a6
xiongjian01 <6960152@qq.com>
2023-01-01 16:58:57 +0800

    Site updated: 2023-01-01 16:58:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/ee6.html","posts/3eeb.html","posts/a16f.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/a16f.html","posts/3eeb.html","posts/ee6.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/20210829.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
