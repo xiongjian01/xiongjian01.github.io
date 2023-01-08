ca026188e333ce82d8e960a9531fc1e33bd9c008
xiongjian01 <6960152@qq.com>
2023-01-08 14:40:35 +0800

    Site updated: 2023-01-08 14:40:35

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "5",
+      "tmpColor": "4C4948",
+      "tmpSize": "16",
+      "cityColor": "4C4948",
+      "citySize": "16",
+      "aqiColor": "4C4948",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10x 100px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "bf2f065bfd8e4e0c914b6aa2f8f44be8"
+    }
+  }
